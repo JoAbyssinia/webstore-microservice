8f2ed8ca6853e1da01546d90a450657c7c3cadb1
bijayshr <bijayshrestha.usa@gmail.com>
2022-05-18 10:39:48 -0500

    Beginning ShoppingCart

--- shoppingcart-command/src/main/java/edu/miu/shoppingcartcommand/entity/Product.java
+++ shoppingcart-command/src/main/java/edu/miu/shoppingcartcommand/entity/Product.java
@@ -1,4 +1,14 @@
 package edu.miu.shoppingcartcommand.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Product {
+    private String productNumber;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
 }
